2891fdf74698cd2be10dfbf2a488936006b6912a
Felipe Aguiar <feliperdaguiar@gmail.com>
2020-01-25 18:53:37 -0300

    Implementando layout responsivo na tela de pesquisa de lançamento

--- financeiro-web/src/app/main/lancamento/lancamento.module.ts
+++ financeiro-web/src/app/main/lancamento/lancamento.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { LancamentoRoutingModule } from './lancamento-routing.module';
-import {InputTextModule} from 'primeng/inputtext';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     LancamentoRoutingModule,
+    SharedModule,
     InputTextModule,
-    SharedModule
+    TableModule,
+    ButtonModule,
+    TooltipModule
   ]
 })
 export class LancamentoModule { }
